dd79b20dadc9162a7c4f1d5c7330c32c029d8e52
Lewis Rotchell <rotchell92@gmail.com>
2021-02-28 18:04:47 +0000

    Connected database

--- server.js
+++ server.js
@@ -8,9 +8,25 @@ process.on("uncaughtException", (err) => {
   process.exit(1);
 });
 
+dotenv.config({ path: "./config.env" });
+
 const app = require("./app");
 
-const PORT = 5000;
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connection successful"));
+
+const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
