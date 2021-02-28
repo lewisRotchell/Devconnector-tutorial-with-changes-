049924f5ecc7bed6d86c059123071a6b350ac2e2
Lewis Rotchell <rotchell92@gmail.com>
2021-02-28 19:29:25 +0000

    Created routes and controller boilerplate

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const express = require("express");
+
+const userRouter = require("./routes/userRoutes");
 const app = express();
+
+//Middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+app.use("/api/users", userRouter);
 
 module.exports = app;
