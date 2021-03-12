c3b34572a13febdbf92378d3d5370cc73fa703f6
Lewis Rotchell <rotchell92@gmail.com>
2021-03-12 10:18:56 +0000

    initialised redux

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
