8d1ee5fcaad592f0b36c0171a58efa4bbe14ab37
Lewis Rotchell <rotchell92@gmail.com>
2021-03-16 15:30:43 +0000

    commit

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -10,17 +10,88 @@ import ProfileEducation from "./ProfileEducation";
 import ProfileGithub from "./ProfileGithub";
 const Profile = ({ match }) => {
   const dispatch = useDispatch();
-  const profile = useSelector((state) => state.profile.profile);
+
+  const profile = useSelector((state) => state.profile.profile.data);
   const loading = useSelector((state) => state.profile.loading);
   const auth = useSelector((state) => state.auth);
 
-  console.log(profile);
-
   useEffect(() => {
     dispatch(getProfileById(match.params.id));
-  }, [dispatch]);
-  return "";
-  // <div>{profile.experience > 0 ? <div>exp</div> : <div>No exp</div>}</div>
+  }, [dispatch, match.params.id]);
+
+  // if (profile) {
+  //   console.log(profile.data);
+  //   return (
+  //     <Fragment>
+  //       <Link to="/profiles" className="btn btn-light">
+  //         Back To Profiles
+  //       </Link>
+  //     </Fragment>
+  //   );
+
+  // } else return <Spinner />;
+
+  return (
+    <Fragment>
+      {profile === null ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <Link to="/profiles" className="btn btn-light">
+            Back To Profiles
+          </Link>
+          {auth.isAuthenticated &&
+            auth.loading === false &&
+            auth.user._id === profile.user._id && (
+              <Link to="/edit-profile" className="btn btn-dark">
+                Edit Profile
+              </Link>
+            )}
+          <div className="profile-grid my-1">
+            <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
+            <div className="profile-exp bg-white p-2">
+              <h2 className="text-primary">Experience</h2>
+              {profile.experience.length > 0 ? (
+                <Fragment>
+                  {profile.experience.map((experience) => (
+                    <ProfileExperience
+                      key={experience._id}
+                      experience={experience}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No experience credentials</h4>
+              )}
+            </div>
+
+            <div className="profile-edu bg-white p-2">
+              <h2 className="text-primary">Education</h2>
+              {profile.education.length > 0 ? (
+                <Fragment>
+                  {profile.education.map((education) => (
+                    <ProfileEducation
+                      key={education._id}
+                      education={education}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No education credentials</h4>
+              )}
+            </div>
+
+            {profile.githubusername && (
+              <ProfileGithub username={profile.githubusername} />
+            )}
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
 export default Profile;
+
+// Hi Will, thanks for writing back. It's my fault, I deleted most of the code before pushing it to github in order to try to strip it back and understand why I was getting the error.
